d237965695dc5096c9faf9fbe6250199b446fa98
quineshabaldwin13 <baldwinquinesha@gmailcom>
2021-09-21 15:27:08 -0400

    Update fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')();
+
+const num1 = prompt('Enter a number');
+console.log(num1);
+
+const num2 = 15;
+
+if(num2 / 3) {
+    console.log('fizz');
+}
+if(num2 / 5) {
+    console.log('buzz');
+}
+
+if(num2 / 5 , 3){
+    console.log('fizzbuzz');
+}
